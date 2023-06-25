c67b4bab9de01fe87a480bc1827d9148c7e4f0bd
Spencer Mullinix <spencermullinix@gmail.com>
2023-06-24 19:06:28 -0600

    added spi-py

---
+++ nfc/nfc.py
@@ -0,0 +1,15 @@
+######################################################
+# The goal is to set up basic nfc features
+# - read value from sensor
+
+
+# Using this tutorial : 
+# - https://tutorials-raspberrypi.com/reading-out-raspberry-pi-rfid-rc522-tags-nfc/
+######################################################
+
+def initialize_sensor:
+
+
+def read_value:
+
+
